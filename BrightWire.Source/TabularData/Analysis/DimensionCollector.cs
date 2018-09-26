aa434953f1e8c7c4515208f8ae5d48800da15250
Jack Dermody <jack@worldtcg.net>
2018-09-27 05:18:28 +1000

    removed distinct couts from dimension analysis

--- BrightWire.Source/TabularData/Analysis/DimensionCollector.cs
+++ BrightWire.Source/TabularData/Analysis/DimensionCollector.cs
@@ -7,8 +7,6 @@ namespace BrightWire.TabularData.Analysis
 {
 	class DimensionCollector : IRowProcessor, IDimensionsColumnInfo
 	{
-		readonly HashSet<string> _distinct = new HashSet<string>();
-
 		public DimensionCollector(int index)
 		{
 			ColumnIndex = index;
@@ -20,35 +18,31 @@ namespace BrightWire.TabularData.Analysis
 
 		public int? ZDimension { get; set; }
 
-		public int ColumnIndex {get;}
+		public int ColumnIndex { get; }
 
 		public IEnumerable<object> DistinctValues => throw new NotImplementedException();
 
-		public int? NumDistinct => _distinct.Count;
+		public int? NumDistinct => null;
 
 		public bool Process(IRow row)
 		{
 			var obj = row.Data[ColumnIndex];
 			if (obj is FloatVector vector) {
-				_distinct.Add($"{vector.Size}");
 				if (XDimension == null || vector.Size > XDimension)
 					XDimension = vector.Size;
-			}else if (obj is FloatMatrix matrix) {
-				_distinct.Add($"{matrix.RowCount},{matrix.ColumnCount}");
+			} else if (obj is FloatMatrix matrix) {
 				if (XDimension == null || matrix.ColumnCount > XDimension)
 					XDimension = matrix.ColumnCount;
 				if (YDimension == null || matrix.RowCount > YDimension)
 					XDimension = matrix.RowCount;
-			}else if (obj is FloatTensor tensor) {
-				_distinct.Add($"{tensor.RowCount},{tensor.ColumnCount},{tensor.Depth}");
+			} else if (obj is FloatTensor tensor) {
 				if (XDimension == null || tensor.ColumnCount > XDimension)
 					XDimension = tensor.ColumnCount;
 				if (YDimension == null || tensor.RowCount > YDimension)
 					XDimension = tensor.RowCount;
 				if (ZDimension == null || tensor.Depth > ZDimension)
 					ZDimension = tensor.Depth;
-			}
-			else
+			} else
 				throw new NotImplementedException();
 
 			return true;
