66fa6e4e83d8d0141b9d3a30348d8f1c55b20bf0
Jack Dermody <jack@worldtcg.net>
2025-11-12 06:28:21 +1100

    updated cuda kernels

--- BrightData.Cuda/cuda/build_kernels.bat
+++ BrightData.Cuda/cuda/build_kernels.bat
@@ -12,5 +12,10 @@ nvcc brightwire.cu -use_fast_math -ptx -m 64 -arch sm_86 -o brightwire_86.ptx -a
 nvcc brightwire.cu -use_fast_math -ptx -m 64 -arch sm_87 -o brightwire_87.ptx -allow-unsupported-compiler
 nvcc brightwire.cu -use_fast_math -ptx -m 64 -arch sm_89 -o brightwire_89.ptx -allow-unsupported-compiler
 nvcc brightwire.cu -use_fast_math -ptx -m 64 -arch sm_90 -o brightwire_90.ptx -allow-unsupported-compiler
+nvcc brightwire.cu -use_fast_math -ptx -m 64 -arch sm_100 -o brightwire_100.ptx -allow-unsupported-compiler
+nvcc brightwire.cu -use_fast_math -ptx -m 64 -arch sm_100 -o brightwire_103.ptx -allow-unsupported-compiler
+nvcc brightwire.cu -use_fast_math -ptx -m 64 -arch sm_100 -o brightwire_110.ptx -allow-unsupported-compiler
+nvcc brightwire.cu -use_fast_math -ptx -m 64 -arch sm_100 -o brightwire_120.ptx -allow-unsupported-compiler
+nvcc brightwire.cu -use_fast_math -ptx -m 64 -arch sm_100 -o brightwire_121.ptx -allow-unsupported-compiler
 
 nvcc brightwire.cu -use_fast_math -ptx -m 64 -arch sm_52 -o brightwire.ptx -allow-unsupported-compiler
